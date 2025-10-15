1b5d145a11e1d9164a891f44c1bf8518477c2a7a
prasenjitpriyan <prasenjitpriyan@gmail.com>
2025-10-15 11:31:38 +0530

    Login page created and linked to home route

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
+import Button from '@/components/Button';
+
 const Home = () => {
   return (
-    <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+    <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 text-3xl text-center md:text-4xl lg:text-7xl space-y-6">
       <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
         Question on React
       </p>
+
+      <Button
+        href="/login"
+        text="Get Started"
+        className="text-base md:text-lg"
+      />
     </div>
   );
 };
