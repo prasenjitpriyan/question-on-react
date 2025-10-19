a2a8d1031b289f6786b7f21e77f0235ab8278ae5
prasenjitpriyan <prasenjitpriyan@gmail.com>
2025-10-19 18:54:01 +0530

    Background UI component deleted and simply add bg-black/80 as default color in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import Providers from '@/components/Providers';
-import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation';
 import { AuthProvider } from '@/lib/auth-context';
+import { Analytics } from '@vercel/analytics/next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -23,23 +23,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`overflow-y-auto hide-scrollbar ${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`overflow-y-auto hide-scrollbar ${geistSans.variable} ${geistMono.variable} antialiased bg-black/80`}>
         <Providers>
           <AuthProvider>
-            <BackgroundGradientAnimation
-              gradientBackgroundStart="rgb(0, 17, 82)"
-              gradientBackgroundEnd="rgb(108, 0, 162)"
-              firstColor="18, 113, 255"
-              secondColor="80, 200, 255"
-              thirdColor="221, 74, 255"
-              fourthColor="200, 50, 100"
-              fifthColor="100, 220, 255"
-              pointerColor="140, 100, 255"
-              size="90%"
-              blendingValue="hard-light"
-              interactive={true}>
-              {children}
-            </BackgroundGradientAnimation>
+            {children}
+            <Analytics />
           </AuthProvider>
         </Providers>
       </body>
